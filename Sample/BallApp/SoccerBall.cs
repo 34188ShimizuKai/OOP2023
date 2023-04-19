2cfb6a74d4901a74aaeefc7990d24493b57e5d19
清水　海 <OIBC34188@ojs.ac.jp>
2023-04-19 10:09:27 +0900

    新規作成

---
+++ Sample/BallApp/SoccerBall.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallApp {
+    class SoccerBall {
+        //フィールド
+        private Image image;//画像データ
+
+        private double posX;//X座標
+        private double posY;//Y座標
+
+        private double moveX = 10.0;//移動量（X方向）
+        private double moveY = 10.0;//移動量（Y方向）
+
+        //コンストラクタ
+        public SoccerBall() {
+            Image = Image.FromFile(@"pic\soccer_ball.png");
+            PosX = 0.0;
+            PosY = 0.0;
+        }
+        //プロパティ
+        public double PosX { get => posX; set => posX = value; }
+        public double PosY { get => posY; set => posY = value; }
+        public Image Image { get => image; set => image = value; }
+
+        //メソッド
+        public void Move() {
+            posX += moveX;
+            Console.WriteLine("座標X："　+ posX);
+            posY += moveY;
+            Console.WriteLine("座標Y：" + posY);
+           
+            if (PosX >= 750 || PosX < 0){
+                moveX *= -1;
+            }else if(PosY >= 530 || PosY < 0){
+                moveY *= -1;
+            }
+        }
+    }
+}
