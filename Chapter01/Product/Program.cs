2723cca20b736ea6a9e02b446d53b033e2f48bf8
清水　海 <OIBC34188@ojs.ac.jp>
2023-05-08 10:55:17 +0900

    p26まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    
+    //商品クラス
+    class Program {
+        static void Main(string[] args) {
+            Product karinto = new Product(123,"かりんとう",180);//インスタンスの生成
+            Product daifuku = new Product(235, "大福もち", 160);
+
+        
+            Console.WriteLine("かりんとうの税込価格：" + karinto.GetPriceIncludingTax());
+         
+            Console.WriteLine("大福もちの税込価格：" + daifuku.GetPriceIncludingTax());}
+    }
+}
