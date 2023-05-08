22ca86f0b5062b769e88e53e35e746dfbf91f82e
清水　海 <OIBC34188@ojs.ac.jp>
2023-05-08 11:42:10 +0900

    0508演習1

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -5,16 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ProductSample {
-    
+
     //商品クラス
     class Program {
         static void Main(string[] args) {
-            Product karinto = new Product(123,"かりんとう",180);//インスタンスの生成
-            Product daifuku = new Product(235, "大福もち", 160);
+            #region P26のサンプルプログラム
+            //Product karinto = new Product(123,"かりんとう",180);//インスタンスの生成
+            //Product daifuku = new Product(235, "大福もち", 160);
 
-        
-            Console.WriteLine("かりんとうの税込価格：" + karinto.GetPriceIncludingTax());
-         
-            Console.WriteLine("大福もちの税込価格：" + daifuku.GetPriceIncludingTax());}
+
+            //Console.WriteLine("かりんとうの税込価格：" + karinto.GetPriceIncludingTax());
+
+            //Console.WriteLine("大福もちの税込価格：" + daifuku.GetPriceIncludingTax());}
+            #endregion
+
+            //DateTime date = new DateTime(2023, 5, 8);
+
+            DateTime date = DateTime.Today;
+            Console.WriteLine("今日の日付:" + date.Year + "年" + date.Month + "月" + date.Day + "日");
+
+            //10日後を求める
+            DateTime daysAfter10 = date.AddDays(10);
+            Console.WriteLine("10日後:" + daysAfter10.Year + "年" + daysAfter10.Month + "月" + daysAfter10.Day + "日");
+
+            //10日前を求める
+            DateTime daysBefore10 = date.AddDays(-10);
+            Console.WriteLine("10日前:" + daysBefore10.Year + "年" + daysBefore10.Month + "月" + daysBefore10.Day + "日");
+        }
     }
 }
