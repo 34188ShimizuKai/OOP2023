7aaf7c30bb093d963a0b8b614b06fb9d83d99103
清水　海 <OIBC34188@ojs.ac.jp>
2023-05-09 09:33:39 +0900

    0508演習2

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -19,18 +19,40 @@ namespace ProductSample {
             //Console.WriteLine("大福もちの税込価格：" + daifuku.GetPriceIncludingTax());}
             #endregion
 
+            #region 演習1
             //DateTime date = new DateTime(2023, 5, 8);
 
-            DateTime date = DateTime.Today;
-            Console.WriteLine("今日の日付:" + date.Year + "年" + date.Month + "月" + date.Day + "日");
+            //DateTime date = DateTime.Today;
+            //Console.WriteLine("今日の日付:" + date.Year + "年" + date.Month + "月" + date.Day + "日");
 
             //10日後を求める
-            DateTime daysAfter10 = date.AddDays(10);
-            Console.WriteLine("10日後:" + daysAfter10.Year + "年" + daysAfter10.Month + "月" + daysAfter10.Day + "日");
+            //DateTime daysAfter10 = date.AddDays(10);
+            //Console.WriteLine("10日後:" + daysAfter10.Year + "年" + daysAfter10.Month + "月" + daysAfter10.Day + "日");
 
             //10日前を求める
-            DateTime daysBefore10 = date.AddDays(-10);
-            Console.WriteLine("10日前:" + daysBefore10.Year + "年" + daysBefore10.Month + "月" + daysBefore10.Day + "日");
+            //DateTime daysBefore10 = date.AddDays(-10);
+            //Console.WriteLine("10日前:" + daysBefore10.Year + "年" + daysBefore10.Month + "月" + daysBefore10.Day + "日");
+            #endregion
+
+            Console.WriteLine("誕生日を入力");
+            Console.Write("西暦:");
+            String bYear = Console.ReadLine();
+            Console.Write("月:");
+            String bMonth = Console.ReadLine();
+            Console.Write("日:");
+            String bDay = Console.ReadLine();
+
+            var bTmp = bYear + bMonth + bDay;
+
+            var birthDay = new DateTime(int.Parse(bYear),int.Parse(bMonth), int.Parse(bDay));
+            //Console.WriteLine(birthDay);
+
+            DateTime dateTo = DateTime.Today;
+            double interval = (dateTo - birthDay).TotalDays;
+            Console.WriteLine("あなたは生まれてから{0}日目です。", interval);
+
+
+
         }
     }
 }
