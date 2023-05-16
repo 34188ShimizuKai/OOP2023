0b1b21bf74098e6d03c052e938696c5979d653ed
清水　海 <OIBC34188@ojs.ac.jp>
2023-05-16 10:39:51 +0900

    コンストラクタ修正

--- Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
@@ -8,11 +8,10 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter(SalesCounter.ReadSales(@"data\sales.csv"));
-
+            SalesCounter sales = new SalesCounter(@"data\sales.csv");
             Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string, int> obj in amountPerStore)
-            {
+            
+            foreach (KeyValuePair<string, int> obj in amountPerStore) {
                 Console.WriteLine("{0} {1:#,0}", obj.Key, obj.Value);
             }
 
--- Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
@@ -10,15 +10,14 @@ namespace SalesCalculator {
         private List<Sale> _sales;
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
         }
 
         //店舗別売り上げを求める
         public Dictionary<String, int> GetPerStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
-            foreach (Sale sale in _sales)
-            {
+            foreach (Sale sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName))
                     dict[sale.ShopName] += sale.Amount;//店名が既に存在する（売り上げ加算）
                 else
@@ -27,18 +26,16 @@ namespace SalesCalculator {
             return dict;
         }
         //売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        public static List<Sale> ReadSales(string filePath) {
+        private List<Sale> ReadSales(string filePath) {
             List<Sale> sales = new List<Sale>();//売り上げデータを格納する
             string[] lines = File.ReadAllLines(filePath);//ファイルからすべてのデータを読み込む
 
 
-            foreach (string line in lines)
-            {//すべての行から１行ずつ取り出す
+            foreach (string line in lines) {//すべての行から１行ずつ取り出す
                 string[] items = line.Split(',');//区切りで項目別に分ける
 
 
-                Sale sale = new Sale
-                {//Saleインスタンスを生成　
+                Sale sale = new Sale {//Saleインスタンスを生成　
                     ShopName = items[0],
                     ProductCategory = items[1],
                     Amount = int.Parse(items[2])
