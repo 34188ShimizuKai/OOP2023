852412d3b22fe796c713ea2854e29fe93761475e
清水　海 <OIBC34188@ojs.ac.jp>
2023-05-16 10:21:37 +0900

    ReadSalesメソッドをSalesCounterクラスへ移動

--- Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,15 +15,38 @@ namespace SalesCalculator {
         }
 
         //店舗別売り上げを求める
-        public Dictionary<String,int> GetPerStoreSales() {
+        public Dictionary<String, int> GetPerStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
-            foreach(Sale sale in _sales){
-                if (dict.ContainsKey(sale.ShopName)) 
+            foreach (Sale sale in _sales)
+            {
+                if (dict.ContainsKey(sale.ShopName))
                     dict[sale.ShopName] += sale.Amount;//店名が既に存在する（売り上げ加算）
                 else
                     dict[sale.ShopName] = sale.Amount; //店名が存在しない（新規格納）
-            }            
+            }
             return dict;
+        }
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        public static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();//売り上げデータを格納する
+            string[] lines = File.ReadAllLines(filePath);//ファイルからすべてのデータを読み込む
+
+
+            foreach (string line in lines)
+            {//すべての行から１行ずつ取り出す
+                string[] items = line.Split(',');//区切りで項目別に分ける
+
+
+                Sale sale = new Sale
+                {//Saleインスタンスを生成　
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);//Saleインスタンスをコレクションに追加
+            }
+
+            return sales;
         }
     }
 }
