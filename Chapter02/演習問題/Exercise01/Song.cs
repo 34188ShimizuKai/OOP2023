337c7925a829b1f87419f5a0bac48b35907de9a9
清水　海 <OIBC34188@ojs.ac.jp>
2023-05-17 11:22:01 +0900

    P79 問題2,1完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //2.1.1
+    class Song {
+        //歌タイトル
+        public string Title{ get; set; }
+        //アーティスト名
+        public string ArtistName { get; set; }
+        //演奏時間（秒）
+        public int Length { get; set; }
+        //2.1.2
+        public Song(string title,string artistname,int length) {
+            Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
+    }
+}
