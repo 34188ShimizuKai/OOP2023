242887f2aaf5c1a39b15acbdf25aabe302c4c4c1
清水　海 <OIBC34188@ojs.ac.jp>
2023-05-22 11:58:20 +0900

    ラムダ式以前（デリゲート）

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 
 namespace Section01 {
     class Program {
+
         static void Main(string[] args) {
-            Console.WriteLine(Count(5));
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            int count = Count(numbers,delegate(int n){ return n % 2 == 0; });
+            Console.WriteLine(count);
         }
-        static public int Count(int num) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 1, 0, 5, 10, 4 };
+        
+        static public int Count(int[] numbers, Predicate<int> judge) {
+
             int count = 0;
 
-            foreach (var i in numbers) {
-                if (i == num)
-                {
+            foreach (var n in numbers) {
+                if (judge(n) == true){
                     count++;
                 }
             }
