278c85a20e60a38a9c0354793f1ec7e6edf80d2e
清水　海 <OIBC34188@ojs.ac.jp>
2023-05-22 11:22:00 +0900

    Section01

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(Count(5));
+        }
+        static public int Count(int num) {
+            var numbers = new[] { 5, 3, 9, 6, 7, 1, 0, 5, 10, 4 };
+            var count = 0;
+            //for (int i = 0; i < numbers.Length; i++){
+            foreach (var i in numbers) { 
+                if(numbers[i] == num)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
