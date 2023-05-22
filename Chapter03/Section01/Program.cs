7444dc5a584a911738a7d4f9a9864894aa5409e9
清水　海 <OIBC34188@ojs.ac.jp>
2023-05-22 11:24:23 +0900

    修正後

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -11,11 +11,13 @@ namespace Section01 {
         }
         static public int Count(int num) {
             var numbers = new[] { 5, 3, 9, 6, 7, 1, 0, 5, 10, 4 };
-            var count = 0;
-            //for (int i = 0; i < numbers.Length; i++){
-            foreach (var i in numbers) { 
-                if(numbers[i] == num)
+            int count = 0;
+
+            foreach (var i in numbers) {
+                if (i == num)
+                {
                     count++;
+                }
             }
             return count;
         }
