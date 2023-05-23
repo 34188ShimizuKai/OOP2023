04cfc11e160a6c1fac667e2cf7aa20bbb6462b7b
清水　海 <OIBC34188@ojs.ac.jp>
2023-05-23 11:12:12 +0900

    P90まで

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -8,8 +8,23 @@ namespace Section03 {
     class Program {
         static void Main(string[] args) {
             var list = new List<string> {
-               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+               "Tokyo", "New Delhi", "Bangkok", "London",
+                "Paris", "Berlin", "Canberra", "Hong Kong",
             };
+            #region 使用例
+            //var exists = list.Exists(s => s[4] == ' ');
+            //Console.WriteLine(exists);
+
+
+            //var name = list.Find(s => s.Length == 6);
+            //Console.WriteLine(name);
+
+            //var index = list.FindIndex(s => s == "Canberra");
+            //Console.WriteLine(index);
+            #endregion
+
+            var names = list.ConvertAll(s => s.ToLower());
+            names.ForEach(s => Console.WriteLine(s));
         }
     }
 }
