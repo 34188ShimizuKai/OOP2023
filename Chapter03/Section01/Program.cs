d38a7ea1cdfb0e49b5abb6b770634dbb27d1f948
清水　海 <OIBC34188@ojs.ac.jp>
2023-05-23 10:30:46 +0900

    P87まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,9 +8,13 @@ namespace Section01 {
     class Program {
 
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            int count = numbers.Count(n => n % 2 == 0);
-            Console.WriteLine(count);
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 ,10, 55, 10, 15, 5};
+
+            //5の倍数(自然数)
+            //var count = numbers.Count(n => n % 5 == 0　&& n > 0);
+            var sum = numbers.Where(n => n % 2 == 0).Average();
+
+            Console.WriteLine(sum);
         }
 
     }
