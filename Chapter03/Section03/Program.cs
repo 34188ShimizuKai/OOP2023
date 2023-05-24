aa86332dac946f5e1669d9c3758fe74f45a875f1
清水　海 <OIBC34188@ojs.ac.jp>
2023-05-24 11:52:36 +0900

    問題3.2 仮

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -12,16 +12,17 @@ namespace Section03 {
                 "Paris", "Berlin", "Canberra", "Hong Kong",
             };
             #region 使用例
-            //var exists = list.Exists(s => s[4] == ' ');
-            //Console.WriteLine(exists);
+            var exists = list.Exists(s => s[4] == ' ');
+            Console.WriteLine(exists);
 
 
-            //var name = list.Find(s => s.Length == 6);
-            //Console.WriteLine(name);
+            var name = list.Find(s => s.Length == 6);
+            Console.WriteLine(name);
 
-            //var index = list.FindIndex(s => s == "Canberra");
-            //Console.WriteLine(index);
+            var index = list.FindIndex(s => s == "Canberra");
+            Console.WriteLine(index);
             #endregion
+
 
             var names = list.ConvertAll(s => s.ToLower());
             names.ForEach(s => Console.WriteLine(s));
---
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var names = new List<string> {
+                 "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+            Console.WriteLine("***** 3.1 *****");
+            Exercise2_1(names);
+            Console.WriteLine();
+            Console.WriteLine("***** 3.2 *****");
+            Exercise2_2(names);
+            Console.WriteLine();
+            Console.WriteLine("***** 3.3 *****");
+            Exercise2_3(names);
+            Console.WriteLine();
+            Console.WriteLine("***** 3.4 *****");
+            Exercise2_4(names);
+        }
+
+        private static void Exercise2_1(List<string> names) {
+            Console.WriteLine("都市名を入力。空行で終了");
+            do {
+                var search = Console.ReadLine();
+                if (string.IsNullOrEmpty(search))
+                    break;
+                if (names.Exists(s => s == search) == true) 
+                    Console.WriteLine(names.FindIndex(s => s == search));
+                else 
+                    Console.WriteLine("-1");
+            } while (true);
+        }
+
+        private static void Exercise2_2(List<string> names) {
+            Console.WriteLine(names.Count(s => s[0] == 'T'));
+        }
+
+        private static void Exercise2_3(List<string> names) {
+            var list = names.Where(s => s.Contains('o')).ToList();
+            foreach (var item in list)
+                Console.WriteLine(item);
+        }
+
+        private static void Exercise2_4(List<string> names) {
+            var list = names.Where(s => s[0] == 'B').ToList();
+            //var length = list.Select(s => s.Length);
+            foreach (var item in list.Select(s => s.Length))
+                Console.WriteLine(item);
+        }
+    }
+}
