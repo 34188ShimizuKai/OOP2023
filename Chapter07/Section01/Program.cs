6a384d9b5c3f6a38381a476e9aa0a43aa406027c
清水　海 <OIBC34188@ojs.ac.jp>
2023-06-22 11:19:32 +0900

    重複考慮済み

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -7,6 +7,66 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
+            #region 例
+            /*var flowerDict = new Dictionary<string, int>()
+            {
+                ["sunflower"] = 400,
+                ["pansy"] = 300,
+                ["tulip"] = 350,
+                ["rose"] = 500,
+                ["dahlia"] = 450,
+            };
+            //flowerDict["morning glory"] = 250;
+            String name = Console.ReadLine();
+            int price = Console.Read();
+
+            flowerDict.Add(name, price);
+
+            //flowerDict.Add("morning glory",250);
+
+            Console.WriteLine("ひまわりの価格は{0}円です。",flowerDict["sunflower"]);
+            Console.WriteLine("チューリップの価格は{0}円です。", flowerDict["tulip"]);
+            Console.WriteLine("アサガオの価格は{0}円です。", flowerDict["morning glory"]);*/
+            #endregion
+
+            var prefDict = new Dictionary<string, string>();
+
+            Console.WriteLine("※終了時は県名に「999」を入力");
+            Console.Write("県名:");
+            string pref = Console.ReadLine();
+            string capital;
+            string duplication;
+
+            while (pref != ("999") && pref != ("９９９")) {
+                Console.Write("所在地:");
+                capital = Console.ReadLine();
+
+                if (prefDict.ContainsKey(pref)) {
+                    Console.WriteLine("既に格納された県名が入力されています。");
+                    Console.Write("上書きしますか？(はい:0,いいえ:1):");
+                    duplication = Console.ReadLine();
+
+                    if (duplication == "0" || duplication == "０") {
+                        prefDict[pref] = capital;
+                    }
+                } else {
+                    prefDict.Add(pref, capital);
+                }
+                Console.WriteLine();
+                Console.Write("県名:");
+                pref = Console.ReadLine();
+
+            }
+
+            Console.WriteLine();
+            Console.Write("県名を入力:");
+            string searcher = Console.ReadLine();
+
+            if (prefDict.ContainsKey(searcher)) {
+                Console.WriteLine("{0}です。", prefDict[searcher]);
+            } else {
+                Console.WriteLine("{0}のデータは存在しません。",searcher);
+            }
         }
     }
 }
