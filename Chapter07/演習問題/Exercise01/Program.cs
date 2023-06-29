b2cc3d44c1a3b54498c47f8d40506be7b68f0fe8
清水　海 <OIBC34188@ojs.ac.jp>
2023-06-29 11:18:14 +0900

    7.1.1完成

---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            Exercise1_1(text);
+            Console.WriteLine();
+            Exercise1_2(text);
+
+
+        }
+
+        private static void Exercise1_1(string text) {
+            var alphabetDict = new Dictionary<Char, int>();
+            foreach (var alphabet in text.ToUpper().Where(s => s != ' '))
+            {
+
+                if (alphabetDict.ContainsKey(alphabet))
+                {
+                    alphabetDict[alphabet] += 1;
+                }
+                else
+                {
+
+                    alphabetDict[alphabet] = 1;
+                }
+            }
+
+            foreach (var item in alphabetDict.OrderBy(a => a.Key))
+            {
+                Console.WriteLine("{0}:{1}",item.Key,item.Value);
+            }
+        }
+
+        private static void Exercise1_2(string text) {
+ 
+        }
+    }
+}
