61f478520641690ae5300077f4e83c531ab6ff59
清水　海 <OIBC34188@ojs.ac.jp>
2023-07-03 14:16:28 +0900

    Exercise02仮

---
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                DateTime nextweek = NextDay(DateTime.Today,(DayOfWeek)dayofweek);
+                Console.WriteLine("{0}の次週の{1}：{2}",DateTime.Today.ToString("yyyy/MM/dd(ddd)"),
+                    nextweek.ToString("ddd曜日"),nextweek.ToString("yyyy/MM/dd(ddd)"));
+            }
+        }
+
+        public static DateTime NextDay(DateTime date, DayOfWeek dayOfWeek) {
+            var days = (int)dayOfWeek - (int)(date.DayOfWeek);
+                days += 7;
+            return date.AddDays(days);
+        }
+
+    }
+}
