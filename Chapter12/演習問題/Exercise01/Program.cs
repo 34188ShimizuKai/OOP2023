43fffec6dfe6f603895c8ff5cf3497a70b8e5460
清水　海 <OIBC34188@ojs.ac.jp>
2023-09-05 15:25:35 +0900

    ex1

--- Chapter12/演習問題/Exercise01/Program.cs
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -50,18 +52,65 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_2(string v) {
-
+            var emps = new Employee[] {
+                new Employee {
+                    Id = 00002,
+                    Name = "食蜂操祈",
+                    HireDate = new DateTime(2004, 12, 1)
+                },
+                new Employee
+                {
+                    Id = 00003,
+                    Name = "藍花悦",
+                    HireDate = new DateTime(2006, 1, 2)
+                },
+            };
+            var settings = new XmlWriterSettings
+            {
+                Encoding = new System.Text.UTF8Encoding(false),
+                Indent = true,
+                IndentChars = " "
+            };
+            using (var writter = XmlWriter.Create(v, settings))
+            {
+                var serializer = new DataContractSerializer(emps.GetType());
+                serializer.WriteObject(writter, emps);
+            }
 
         }
 
         private static void Exercise1_3(string v) {
-
-
+            using(XmlReader reader = XmlReader.Create(v))
+            {
+                var serializer = new DataContractSerializer(typeof(Employee[]));
+                var emps = serializer.ReadObject(reader) as Employee[];
+                foreach (var emp in emps)
+                {
+                    Console.WriteLine("{0} {1} {2}", emp.Id, emp.Name, emp.HireDate);
+                }
+            }
         }
 
         private static void Exercise1_4(string v) {
-
-
+            var emps = new Employee[]
+            {
+                new Employee
+                {
+                    Id = 00004,
+                    Name = "削板軍覇",
+                    HireDate = new DateTime(2021,9,9)
+                },
+                new Employee
+                {
+                    Id = 00005,
+                    Name = "御坂美琴",
+                    HireDate = new DateTime(2023,1,12)
+                },
+            };
+            using(var stream = new FileStream(v, FileMode.Create, FileAccess.Write)){
+                var serializer = new DataContractJsonSerializer(emps.GetType());
+                serializer.WriteObject(stream, emps);
+            }
         }
     }
 }
