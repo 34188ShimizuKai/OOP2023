08bcfddcb6dadaa7707ba3ba2f9165cf6a629fab
清水　海 <OIBC34188@ojs.ac.jp>
2023-09-26 12:00:39 +0900

    sec2

---
+++ Chapter14/Section02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    class Program {
+        static void Main(string[] args) {
+            var wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            var html = wc.DownloadString("https://visualstudio.microsoft.com/ja/");
+            Console.WriteLine(html);
+        }
+    }
+}
