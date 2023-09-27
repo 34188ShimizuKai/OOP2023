a047319c947111e9a24494b7ac8f1fc1f79ab6d3
清水　海 <OIBC34188@ojs.ac.jp>
2023-09-27 09:22:56 +0900

    zip

--- Chapter14/Section02/Program.cs
+++ Chapter14/Section02/Program.cs
@@ -9,9 +9,23 @@ namespace Section02 {
     class Program {
         static void Main(string[] args) {
             var wc = new WebClient();
-            wc.Encoding = Encoding.UTF8;
-            var html = wc.DownloadString("https://visualstudio.microsoft.com/ja/");
-            Console.WriteLine(html);
+            var url = new Uri(@"https://www.rimarts.jp/downloads/B2/bk28104j.zip");
+            var filename = @"C:\temp\example.zip";
+            wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+            wc.DownloadFileCompleted += wc_DownloadFileCompleted;
+            wc.DownloadFileAsync(url, filename);
+            Console.ReadLine();
+        }
+
+        static void wc_DownloadProgressChanged(object sender,
+                    DownloadProgressChangedEventArgs e) {
+            Console.WriteLine("{0}% {0}/{1}", e.ProgressPercentage,
+                              e.BytesReceived, e.TotalBytesToReceive);
+        }
+
+        static void wc_DownloadFileCompleted(object sender,
+                            System.ComponentModel.AsyncCompletedEventArgs e) {
+            Console.WriteLine("ダウンロード完了");
         }
     }
 }
