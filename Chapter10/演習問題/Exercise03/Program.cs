a85bea83b5c76f9593710364d380bb923df0a961
清水　海 <OIBC34188@ojs.ac.jp>
2023-10-10 09:35:31 +0900

    ex4

---
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var texts = new[] {
+               "Time is money.",
+               "What time is it?",
+               "It will take time.",
+               "We reorganized the timetable.",
+            };
+            var regex = new Regex(@"\b(T|t)ime\b");
+            foreach (var text in texts)
+            {
+                var matches = regex.Matches(text);
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine("{0}:{1}",text,match.Index);
+                }//var isMatch = regex.IsMatch(text);
+                //if (isMatch)
+                //    Console.WriteLine(text);
+            }
+
+
+
+        }
+    }
+}
---
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise04 {
+    class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.txt");
+
+            var newline = lines.Select(s=>Regex.Replace(s,@"\b(v|V)ersion=""v4.0""",@"version=""5.0"""));
+            File.WriteAllLines("sample.txt",newline);
+
+
+
+
+            // これ以降は確認用
+            var text = File.ReadAllText("sample.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
