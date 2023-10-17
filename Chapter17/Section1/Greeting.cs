199ed4e8311a832cae8810019e0608bf04317189
清水　海 <OIBC34188@ojs.ac.jp>
2023-10-17 10:21:18 +0900

    継承を使ったポリモーフィズム

---
+++ Chapter17/Section1/Greeting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section1 {
+    class GreetingMorning : GreetingBase{
+        public override string GetMassage() {
+            return "おはよう";
+        }
+    }
+    class GreetingAfternoon : GreetingBase {
+        public override string GetMassage() {
+            return "こんにちは";
+        }
+    }
+
+    class GreetingEvening : GreetingBase {
+        public override string GetMassage() {
+            return "こんばんは";
+        }
+    }
+}
---
+++ Chapter17/Section1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section1 {
+    class Program {
+        static void Main(string[] args) {
+            var greetings = new List<GreetingBase>()
+            {
+                new GreetingMorning(),
+                new GreetingAfternoon(),
+                new GreetingEvening(),
+            };
+
+            foreach (var obj in greetings)
+            {
+                string msg = obj.GetMassage();
+                Console.WriteLine(msg);
+            }
+        }
+    }
+}
