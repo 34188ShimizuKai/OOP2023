aa5e2ea59e49f19af050183c62cc6b3e964a39bd
清水　海 <OIBC34188@ojs.ac.jp>
2023-11-01 11:52:45 +0900

    進捗

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,26 +21,50 @@ namespace ColorChecker {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            DataContext = GetColorList();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e) {
-
+        }
+        private void setColor() {
+            Color color = Color.FromRgb((byte)redSlider.Value, (byte)greenSlider.Value, (byte)blueSlider.Value);
+            SolidColorBrush brush = new SolidColorBrush(color);
+            colorArea.Background = brush;
         }
 
-        private void redSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
-            colorSet();
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            setColor();
         }
 
-        private void greenSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
-
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
         }
 
-        private void blueSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+        private void colorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var selectedColor = (MyColor)((ComboBox)sender).SelectedItem;
+            var color = selectedColor.Color;
+            var name = selectedColor.Name;
+            colorArea.Background = new SolidColorBrush(color);
 
+            redSlider.Value = (double)color.R;
+            greenSlider.Value = (double)color.G;
+            blueSlider.Value = (double)color.B;
         }
-        private void colorSet() {
-            
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+            string colorList = string.Format("R={0} G={1} B={2}",redSlider.Value,greenSlider.Value,blueSlider.Value);
+            stockList.Items.Add(colorList);
         }
 
+        private void stockList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            string[] split = stockList.SelectedItem.ToString().Split(' ', '=');
+            redValue.Text = split[1];
+            greenValue.Text = split[3];
+            blueValue.Text = split[5];
+        }
+    }
+    public class MyColor {
+        public Color Color { get; set; }
+        public string Name { get; set; }
     }
 }
